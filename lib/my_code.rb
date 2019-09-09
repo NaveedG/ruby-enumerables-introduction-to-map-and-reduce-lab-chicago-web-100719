762d4c4f732d7b9f3466c37540a1346eb1d4974a
Naveed Ganjani <nganjani@protonmail.com>
2019-09-09 20:56:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,7 +39,13 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point= 0)
-  source_array.reduce(starting_point, :+)
+  #source_array.reduce(starting_point, :+)
+  x = 0
+  while source_array.length > x
+    source_array[x] += starting_point
+    x += 1
+  end
+  starting_point
 end
 
 def reduce_to_all_true(source_array)
